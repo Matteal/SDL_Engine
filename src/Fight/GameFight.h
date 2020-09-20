103460fb9817b1d8edae28c6966e38e4c13b9d8d
Matteo <46777160+Sholin4@users.noreply.github.com>
2020-09-20 18:45:18 +0200

    Revert "Merge branch 'dev-Colep'"
    This reverts commit 368d06a8a79e8e310af3994e918dda12dc27a7cb, reversing
    changes made to 89a5b604bb892d2e25cbefc70a4a85f4768a74f4.

--- src/Fight/GameFight.h
+++ src/Fight/GameFight.h
@@ -4,9 +4,6 @@
 #include "../SDL/Scene.h"
 #include "../Bridge.h"
 
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-
 
 class GameFight : public Scene
 {
@@ -16,17 +13,12 @@ class GameFight : public Scene
 
         void update(Input* input);
         void render();
-        void Write(const char* file, int charsize);
 
         Bridge* m_bridge;
 
     protected:
 
     private:
-        TTF_Font* police;
-        Mix_Chunk* strike;
-        Mix_Chunk* surprise;
-        Mix_Chunk* defense;
 };
 
 #endif // GAME_FIGHT_H
--- src/SDL/Window.h
+++ src/SDL/Window.h
@@ -7,7 +7,6 @@
 
 #include <SDL_image.h>
 #include <SDL_mixer.h>
-#include <SDL_ttf.h>
 
 
 
